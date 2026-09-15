15dc80e7d3fdbad0621c2b04894f8545913dab24
ricardopatriciodossantos <ricardo.dossantos@amadeus.com>
2026-09-15 17:38:36 +0100

    Update local package dependencies

--- Package.swift
+++ Package.swift
@@ -17,13 +17,13 @@ let package = Package(
         .package(url: "https://github.com/vbmobile/AMANFCPassportReader", exact: "2.3.3-vb.2"),
         .package(url: "https://github.com/CocoaLumberjack/CocoaLumberjack.git", from: "3.8.0"),
         .package(url: "https://github.com/vbmobile/AMAUtils", .upToNextMinor(from: "3.0.4")),
-        .package(url: "https://github.com/vbmobile/AMANetworkClient.git", .upToNextMinor(from: "7.0.2")),
+        .package(path: "../../AMANetworkClient"),
     ],
     targets: [
         .binaryTarget(
             name: "AMADocRFIDReadiOS",
-            url: "https://vbmobileidstorage.blob.core.windows.net/ios/AMADocRFIDReadiOS/AMADocRFIDReadiOS-3.2.0.zip",
-            checksum: "02cd1abb8ea9c3a54154d51b8931d133ba2fbf9fb2327cae07bf8595f910c9dd"
+            url: "https://vbmobileidstorage.blob.core.windows.net/ios/AMADocRFIDRead/AMADocRFIDReadiOS-3.2.0.zip",
+            checksum: "947f24baf304cd3f446ab6cd03410ef1c00af0a272925b8d166e886237cdf02a"
         ),
         .target(
             name: "AMADocRFIDReadiOSWrapper",
