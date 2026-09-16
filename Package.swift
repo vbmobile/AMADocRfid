4b50132e18e2f0662759ff3640af43b7226da548
Tiago Silva <tiago.silva@vision-box.com>
2026-09-16 13:51:29 +0100

    Update AMADocRFIDReadiOS binary to 3.2.1

--- Package.swift
+++ Package.swift
@@ -16,14 +16,14 @@ let package = Package(
         .package(url: "https://github.com/vbmobile/AMADocModel", .upToNextMinor(from: "3.1.0")),
         .package(url: "https://github.com/vbmobile/AMANFCPassportReader", exact: "2.3.3-vb.2"),
         .package(url: "https://github.com/CocoaLumberjack/CocoaLumberjack.git", from: "3.8.0"),
-        .package(url: "https://github.com/vbmobile/AMAUtils", .upToNextMinor(from: "3.0.4")),
-        .package(path: "../../AMANetworkClient"),
+        .package(url: "https://github.com/vbmobile/AMAUtils", .upToNextMinor(from: "3.0.8")),
+        .package(url: "https://github.com/vbmobile/AMANetworkClient", .upToNextMinor(from: "7.0.4")),
     ],
     targets: [
         .binaryTarget(
             name: "AMADocRFIDReadiOS",
-            url: "https://vbmobileidstorage.blob.core.windows.net/ios/AMADocRFIDRead/AMADocRFIDReadiOS-3.2.0.zip",
-            checksum: "947f24baf304cd3f446ab6cd03410ef1c00af0a272925b8d166e886237cdf02a"
+            url: "https://vbmobileidstorage.blob.core.windows.net/ios/AMADocRFIDReadiOS/AMADocRFIDReadiOS-3.2.1.zip",
+            checksum: "3e73eb909cc5b639d0891e928f9e397a90a6475fd4ac2abb8624924ade1fb62c"
         ),
         .target(
             name: "AMADocRFIDReadiOSWrapper",
